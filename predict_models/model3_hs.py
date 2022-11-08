049cc3f7f4aa34a985ff6d033ee1e8f90786a8ea
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2022-11-08 14:19:17 +0900

    Update model3_hs.py

--- predict_models/model3_hs.py
+++ predict_models/model3_hs.py
@@ -1,7 +1,7 @@
 # 불량 처리 3번 기준 평가 기능
 
-# 입력 : 폴더 경로 (이미지 아님)
-# 출력 : 검사 결과 리스트
-def model3_hs(folderPath):
-    result = []
+# 입력 : 이미지 경로 (이미지 아님)
+# 출력 : 검사 결과 T/F
+def model3_hs(imagePath):
+    result = False
     return result
