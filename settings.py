cba37ca462380393d2587b1f73f27ead5a2899b2
blueberrysmoooothie <jhs852963@naver.com>
2022-11-28 09:42:29 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ UP_FOLER_PATH = "./image/module/"
 # sampe folder path
 test_ok_path = "./image/module/true_ok/GSY827AN7A4002_AAO03151K_PKT04_CM1EQSUA0011_20220712220034_DirectLight_OK.jpg"
 
-
 # sample NG images
 test_np_path1 = "./image/module/true_ng/GSY827AN7B0918_AAO05408K_PKT07_CM1EQSUA0012_20220712000659_DirectLight_NG.jpg"
 test_np_path2 = "./image/module/true_ng/GSY827AN7C4411_AAO31565K_PKT08_CM1EQSUA0011_20220713080822_DirectLight_NG.jpg"
