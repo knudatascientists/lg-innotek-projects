12e5962544e444c758824894a3412dc3ea109fe5
blueberrysmoooothie <93635517+blueberrysmoooothie@users.noreply.github.com>
2022-11-29 17:46:17 +0900

    pyqt

--- pyqt_test.py
+++ pyqt_test.py
@@ -14,6 +14,8 @@ class WindowClass(QMainWindow, from_class):
         super().__init__()
         self.setupUi(self)
 
+        self.menu1_pushed()
+
         self.menu1Button.clicked.connect(self.menu1_pushed)
         self.menu2Button.clicked.connect(self.menu2_pushed)
         self.findPathButton.clicked.connect(self.findPath_pushed)
@@ -22,25 +24,46 @@ class WindowClass(QMainWindow, from_class):
         self.debug = False
         self.cnn_checkBox.stateChanged.connect(self.cnn_change)
         self.debug_checkBox.stateChanged.connect(self.debug_change)
+        self.progressBar.close()
         self.set_directory()
 
     def menu1_pushed(self):
+        self.test_type = "all"
+        self.saveFrame.show()
+        self.logText.clear()
+        self.progressBar.show()
         self.set_directory()
         self.debugView.close()
+
+        self.set_saveDirectory()
+
         self.write_log_text("전체 이미지 검사")
 
     def menu2_pushed(self):
+        self.test_type = "one"
+        self.saveFrame.close()
+        self.logText.clear()
+        self.progressBar.close()
         self.set_directory()
         self.debugView.show()
         self.write_log_text("단일 이미지 검사")
 
     def findPath_pushed(self):
-        folderpath = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if self.test_type == "one":
+            folderpath = QFileDialog.getOpenFileName(self, "Select Folder")[0]
+        else:
+            folderpath = QFileDialog.getExistingDirectory(self, "Select Folder")
         self.set_directory(folderpath)
-        self.write_log_text("경로 가져오기")
+
+    def findSavePath_pushed(self):
+        folderpath = QFileDialog.getExistingDirectory(self, "Select Folder")
+        self.set_saveDirectory(folderpath)
 
     def test_pushed(self):
-        self.write_log_text(f"testing products..., cnn : {self.cnn}, debug : {self.debug}")
+        if self.test_type == "one":
+            self.write_log_text(f"testing {self.folderPath}..., cnn : {self.cnn}")
+        else:
+            self.write_log_text(f"testing {self.folderPath}..., cnn : {self.cnn}, debug : {self.debug}")
 
     def cnn_change(self):
         if self.cnn_checkBox.isChecked():
@@ -54,7 +77,13 @@ class WindowClass(QMainWindow, from_class):
         else:
             self.debug = False
 
+    def set_saveDirectory(self, path=""):
+        self.folderPath = path
+        self.savePathLabel.clear()
+        self.savePathLabel.setText(path)
+
     def set_directory(self, path=FOLDER_PATH):
+        self.folderPath = path
         self.pathLabel.clear()
         self.pathLabel.setText(path)
 
